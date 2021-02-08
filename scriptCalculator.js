c18a2d0e33e678bc1078ae6cb24afeca66b366ee
Pavle <mpavle134@gmail.com>
2021-02-07 21:32:58 -0300

    whole calculator display made, making buttons work in progress

--- scriptCalculator.js
+++ scriptCalculator.js
@@ -1,13 +1,13 @@
-function add(a,b){
+function addFunc(a,b){
     return a+b;
 }
-function subtract(a,b){
+function subtractFunc(a,b){
     return a-b;
 }
-function multiply(a,b){
+function multiplyFunc(a,b){
     return a*b;
 }
-function divide(a,b){
+function divideFunc(a,b){
     return a/b;
 }
 function operate(operator,a,b){
@@ -16,3 +16,32 @@ function operate(operator,a,b){
     if(operator == '-')return subtract(a,b);
     if(operator == '/')return divide(a,b);
 }
+let display = document.querySelector('.display');
+let one = document.querySelector('.one');
+let two = document.querySelector('.two');
+let three = document.querySelector('.three');
+let four = document.querySelector('.four');
+let five = document.querySelector('.five');
+let six = document.querySelector('.six');
+let seven = document.querySelector('.seven');
+let eight = document.querySelector('.eight');
+let nine = document.querySelector('.nine');
+let zero = document.querySelector('.zero');
+let add = document.querySelector('.add');
+let subtract = document.querySelector('.subtract');
+let multiply = document.querySelector('.multiply');
+let divide = document.querySelector('.divide');
+let percentage = document.querySelector('.percentage');
+let dot = document.querySelector('.dot');
+let clear = document.querySelector('.clear');
+let erase = document.querySelector('.erase');
+let equal = document.querySelector('.equal');
+
+let aNumber = {
+    numer =  
+}
+let bNumber = 0;
+
+one.addEventListener('click',(e) => {
+    alert('1');
+})
