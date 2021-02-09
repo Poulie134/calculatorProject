3ce5d1d25228738b4c9101487e49c56933efa514
Pavle <mpavle134@gmail.com>
2021-02-09 13:48:25 -0300

    made state 1 and 2, both working, and changed some classes to make it happen

--- scriptCalculator.js
+++ scriptCalculator.js
@@ -36,12 +36,80 @@ let dot = document.querySelector('.dot');
 let clear = document.querySelector('.clear');
 let erase = document.querySelector('.erase');
 let equal = document.querySelector('.equal');
+let stateDiv = document.querySelector('#container')
 
-let aNumber = {
-    numer =  
+let aNumber =''
+let aNumberInt;
+let bNumber =''
+let bNumberInt;
+let operator = ''
+let dotCounter = 0
+
+function cleareverything(){
+    aNumber = ''
+    bNumber = ''
+    aNumberInt = 0;
+    bNumberInt = 0;
+    operator = ''
+    dotCounter = 0
 }
-let bNumber = 0;
+btns = document.querySelectorAll('.boxy');
+btns.forEach(boxy => {
+    boxy.addEventListener('click',(e) => {
+        //clear button 
+        if(boxy.classList[0]=='clear'){
+            stateDiv.classList.remove('state1','state2','state3','state4')
+            stateDiv.classList.add('state1')
+        }
 
-one.addEventListener('click',(e) => {
-    alert('1');
-})
+        // State 1 made
+        if(stateDiv.classList == 'state1'){
+            if(boxy.classList[2] == 'number'){
+                aNumber += boxy.id
+                display.textContent = aNumber
+            }
+            else if(boxy.classList[2] == 'undo'){
+                if(aNumber.charAt(aNumber.length-1)==".")dotCounter --;
+                aNumber = aNumber.substring(0,aNumber.length-1)
+                display.textContent = aNumber
+            }
+            else if(boxy.classList[0] == 'dot'){
+                if(dotCounter == 0){
+                    aNumber += '.';
+                    display.textContent = aNumber;
+                    dotCounter ++;
+                }
+            }
+            else if(boxy.classList[0] == 'clear'){
+                cleareverything();
+                display.textContent = aNumber;
+            }
+            else if(boxy.classList[2] == 'operator'){
+                if(aNumber == '')return;
+                operator = boxy.id;
+                stateDiv.classList.remove('state1');
+                stateDiv.classList.add('state2');
+                console.log(operator);
+            }         
+        }
+
+        //state 2
+        if(stateDiv.classList == 'state2'){
+            if(boxy.classList[2] == 'operator' ){
+                operator = boxy.id;
+                console.log(operator);
+            }
+            if(boxy.classList[2] == 'number'){
+                stateDiv.classList.remove('state2')
+                stateDiv.classList.add('state3')
+                bNumber += boxy.id
+                display.textContent = bNumber
+            }
+        }
+        //state 3
+        if(stateDiv.classList == 'state3'){
+
+        }
+
+    });
+});
